3d37b582307c2e6bb75add081cd2d4cc69442bfd
Artyom <artemdpavlov84@gmail.com>
2026-04-26 22:19:24 +0300

    Добавил файлы по 5 лабе по программированию

---
+++ lab5_prog/lab5_prog/lab5_prog.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    double n;
+    double s = 0;
+    int i = 1;
+    setlocale(LC_ALL, "Russian");
+    printf("Введите число n: ");
+    scanf_s("%lf", &n);
+    while (s < n)
+    {
+        s = s + 1.0 / i;
+        i++;
+    }
+    printf("Наименьшее число, большее числа %lf: %lf\n", n, s);
+    return 0;
+}
